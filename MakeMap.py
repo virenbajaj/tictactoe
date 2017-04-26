34c92c8637053b457f8c962671a8ef435b97fb4e
ljelenak <l.jelenak@gmail.com>
2017-04-26 15:46:40 -0400

    Update MakeMap.py
    added separate methods to things

--- MakeMap.py
+++ MakeMap.py
@@ -1,14 +1,121 @@
 from math import *
+
 def rotate(x,y,dx,dy,q)
     return (x+dx*cos(q)+dy*sin(q),y-dx*sin(q)+dy*cos(q))
 
-def MakeMap(corner,pose):
+def MapXChips(corner,x,y,q):
     l = 100 # box length
     m = 40  # marker length
     cozmo_width = 100 #mm
     start_diff = 150 #distance used to get start position
     
-    x,y,q = pose #x and y are the position of the markers
+    #x,y,q = pose #x and y are the position of the markers
+                 #q is the rotation of the marker
+    
+    #scale factors for center of different boxes
+    x1 = y1 = 0.5*(m+l) 
+    x2 = y2 = 0.5*(m+3*l)
+    x3 = y3 = 0.5*(m+5*l)
+    
+    x4 = y4 = (m+3*l) #other marker
+    gap = m*0.5+cozmo_width*1.25 #between chips
+    
+    if corner == 1:
+        xchips_pos = [rotate(x,y,gap,-y1,q),
+                      rotate(x,y,2*gap,-y1,q),
+                      rotate(x,y,3*gap,-y1,q),
+                      rotate(x,y,4*gap,-y1,q),
+                      rotate(x,y,5*gap,-y1,q)]
+        xpickup_pos = [rotate(x,y,gap,-y2,q),
+                       rotate(x,y,2*gap,-y2,q),
+                       rotate(x,y,3*gap,-y2,q),
+                       rotate(x,y,4*gap,-y2,q),
+                       rotate(x,y,5*gap,-y2,q)]
+    if corner == 3:
+        xchips_pos = [rotate(x,y,x4-gap,y4+y1,q),
+                      rotate(x,y,x4-2*gap,y4+y1,q),
+                      rotate(x,y,x4-3*gap,y4+y1,q),
+                      rotate(x,y,x4-4*gap,y4+y1,q),
+                      rotate(x,y,x4-5*gap,y4+y1,q)]
+
+        xpickup_pos = [rotate(x,y,x4-gap,y4+y2,q),
+                       rotate(x,y,x4-2*gap,y4+y2,q),
+                       rotate(x,y,x4-3*gap,y4+y2,q),
+                       rotate(x,y,x4-4*gap,y4+y2,q),
+                       rotate(x,y,x4-5*gap,y4+y2,q)]
+    return (xchips_pos,xpickup_pos)
+    
+def MapOChips(corner,x,y,q):
+    l = 100 # box length
+    m = 40  # marker length
+    cozmo_width = 100 #mm
+    #start_diff = 150 #distance used to get start position
+    
+    #x,y,q = pose #x and y are the position of the markers
+                 #q is the rotation of the marker
+    
+    #scale factors for center of different boxes
+    x1 = y1 = 0.5*(m+l) 
+    x2 = y2 = 0.5*(m+3*l)
+    #x3 = y3 = 0.5*(m+5*l)
+    
+    x4 = y4 = (m+3*l) #other marker
+    gap = m*0.5+cozmo_width*1.25 #between chips
+    
+    if corner == 1:
+        ochips_pos = [rotate(x,y,x4-gap,y4+y1,q),
+                      rotate(x,y,x4-2*gap,y4+y1,q),
+                      rotate(x,y,x4-3*gap,y4+y1,q),
+                      rotate(x,y,x4-4*gap,y4+y1,q)]
+        
+        opickup_pos = [rotate(x,y,x4-gap,y4+y2,q),
+                      rotate(x,y,x4-2*gap,y4+y2,q),
+                      rotate(x,y,x4-3*gap,y4+y2,q),
+                      rotate(x,y,x4-4*gap,y4+y2,q)]
+    if corner == 3:
+        ochips_pos = [rotate(x,y,gap,-y1,q),
+                      rotate(x,y,2*gap,-y1,q),
+                      rotate(x,y,3*gap,-y1,q),
+                      rotate(x,y,4*gap,-y1,q)]
+        opickup_pos = [rotate(x,y,gap,-y2,q),
+                      rotate(x,y,2*gap,-y2,q),
+                      rotate(x,y,3*gap,-y2,q),
+                      rotate(x,y,4*gap,-y2,q)]
+    return (ochips_pos,opickup_pos)
+
+
+def MapStart(corner,x,y,q):
+    l = 100 # box length
+    m = 40  # marker length
+    #cozmo_width = 100 #mm
+    start_diff = 150 #distance used to get start position
+    
+    #x,y,q = pose #x and y are the position of the markers
+                 #q is the rotation of the marker
+    
+    #scale factors for center of different boxes
+    #x1 = y1 = 0.5*(m+l) 
+    #x2 = y2 = 0.5*(m+3*l)
+    #x3 = y3 = 0.5*(m+5*l)
+    
+    x4 = y4 = (m+3*l) #other marker
+    #gap = m*0.5+cozmo_width*1.25 #between chips
+    
+    if corner == 1:
+        start1 = rotate(x,y,-start_diff,-start_diff,q)
+        start2 = rotate(x,y,x4+start_diff,y4+start_diff,q)
+    if corner == 3:
+        start2 = rotate(x,y,-start_diff,-start_diff,q)
+        start1 = rotate(x,y,x4+start_diff,y4+start_diff,q)
+    return (start1,start2)
+    
+def MapBoard(corner,x,y,q):
+    l = 100 # box length
+    m = 40  # marker length
+    cozmo_width = 100 #mm
+    start_diff = 150 #distance used to get start position
+    
+    #x,y,q = pose #x and y are the position of the markers
                  #q is the rotation of the marker
     
     #scale factors for center of different boxes
@@ -144,8 +251,8 @@ def MakeMap(corner,pose):
     #                   [(x+x3,y+y1),(x+x3,y+y2),(x+x3,y+y3)]]
 
     board_pose = [[p11,p12,p13],[p21,p22,p23],[p31,p32,p33]]
-    
-    return (board_pose,xchips_pos,ochips_pos,start1,start2,xpickup_pos,opickup_pos)
+    return [p11,p12,p13,p21,p22,p23,p31,p32,p33]
+    #return (board_pose,xchips_pos,ochips_pos,start1,start2,xpickup_pos,opickup_pos)
     print(board_pose)
 
 
